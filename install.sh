eb5a4861d6177c600c08bd3cd15609cbeb057722
joelmiller1 <corvettejm@gmail.com>
2021-02-08 19:27:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-#test install script
+# test install script
+# to run, enter the following command into terminal:
+# curl -sSL https://raw.githubusercontent.com/joelmiller1/pub/main/install.sh | bash
+#
+# this is potentially dangerous, so know the implications of it:
+# https://pi-hole.net/2016/07/25/curling-and-piping-to-bash#page-content
 mkdir temp
 cd temp
-touch hellworld.txt >> hello, world
 echo "test"
