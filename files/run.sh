97da85416ed1a4893424b9d143bced40f7c43e38
joelmiller1 <corvettejm@gmail.com>
2021-02-08 23:14:39 -0500

    Update run.sh

--- files/run.sh
+++ files/run.sh
@@ -6,10 +6,10 @@ function go_outorder {
 date
 echo "starting go in-order=$5 width $1, intalu $2, intmult $3, memprt $4"
 echo "in-order=$5, width=$1, intalu=$2, intmult=$3, memprt=$4" >> execution_time.txt
-/usr/bin/time -a -o ./outorder/runtime.txt $SIM -fetch:ifqsize $1 -decode:width $1 -issue:width $1 -commit:width $1 -res:ialu $2 -res:imult $3 -res:memport $4 -issue:inorder $5 -redir:sim "go_stdout_$1_$2_$3_$4_$5.txt" go.ss 50 9 2stone9.in > go_output_$1_$2_$3_$4_$5.txt
+/usr/bin/time -a -o runtime.txt $SIM -fetch:ifqsize $1 -decode:width $1 -issue:width $1 -commit:width $1 -res:ialu $2 -res:imult $3 -res:memport $4 -issue:inorder $5 -redir:sim "go_stdout_$1_$2_$3_$4_$5.txt" go.ss 50 9 2stone9.in > go_output_$1_$2_$3_$4_$5.txt
 echo "done"
 echo "running diff on go in-order=$5 width $1, intalu $2, intmult $3, memprt $4"
-diff ./outorder/go_output_$1_$2_$3_$4_$5.txt 2stone9.out
+diff go_output_$1_$2_$3_$4_$5.txt 2stone9.out
 echo "done with diff check"
 date
 }
@@ -17,10 +17,10 @@ date
 function go_pred {
 date
 echo "starting go in-order=$1, bpred=$2"
-/usr/bin/time -a -o ./pred/runtime.txt $SIM -fetch:ifqsize 4 -decode:width 4 -issue:width 4 -commit:width 4 -res:ialu 3 -res:imult 2 -res:memport 2 -issue:inorder $1 -bpred $2 -redir:sim "pred_$2_$1.txt" go.ss 50 9 2stone9.in > pred_output_$2_$1.txt
+/usr/bin/time -a -o runtime.txt $SIM -fetch:ifqsize 4 -decode:width 4 -issue:width 4 -commit:width 4 -res:ialu 3 -res:imult 2 -res:memport 2 -issue:inorder $1 -bpred $2 -redir:sim "pred_$2_$1.txt" go.ss 50 9 2stone9.in > pred_output_$2_$1.txt
 echo "done"
 echo "running diff on go in-order=$1, bpred=$2"
-diff ./pred/pred_output_$2_$1.txt 2stone9.out
+diff pred_output_$2_$1.txt 2stone9.out
 echo "done with diff check"
 date
 }
