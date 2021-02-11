58ed823187fd5750be141589046a1b7d0096e24d
joelmiller1 <corvettejm@gmail.com>
2021-02-10 22:02:02 -0500

    Update install_with_dependencies.sh

--- install_with_dependencies.sh
+++ install_with_dependencies.sh
@@ -12,8 +12,40 @@ mkdir lab2
 cd lab2
 
 # check for dependencies
-curl -sSL -O -J https://raw.githubusercontent.com/joelmiller1/pub/main/files/check.sh
-./check.sh
+apt update
+
+# chech for time
+if ! command -v time &> time
+then
+	echo "command 'time' not found"
+	echo "will try and install..."
+	apt install time
+	echo "finished installing time"
+else
+	echo "time command found!"
+fi
+
+# check for make
+if ! command -v make &> /usr/bin/make
+then
+	echo "command 'make' not found"
+	echo "will try and install..."
+	apt install make
+	echo "finished installing make"
+else
+	echo "make command found!"
+fi
+
+# check for gcc/g++
+if ! command -v gcc &> /usr/bin/gcc
+then
+	echo "command 'gcc' not found"
+	echo "will try and install gcc, g++, and support libs..."
+	apt install g++ gcc-multilib g++-multilib
+	echo "finished installing gcc"
+else
+	echo "gcc command found!"
+fi
 
 # download the things
 echo "downloading files to be extracted..."
